b7920798657d3cb41ee61e3783c195240f1a0348
Shu Dong <ShuDong2016@u.northwestern.edu>
2016-03-02 15:47:50 -0600

    code update

---
+++ models.py
@@ -0,0 +1,38 @@
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn import linear_model
+
+class Models():
+    mods = {}
+    multiclass = True
+
+    def __init__(self, models, multiclass=True):
+        self.multiclass = multiclass
+        for m in models:
+           self.mods[m] = self.create_model(m, multiclass)
+
+    def create_model(self, name, multiclass=True):
+        if name == "nb":
+            return MultinomialNB()
+        elif name == "svm":
+            return OneVsRestClassifier(SVC()) if multiclass else SVC()
+        elif name == "lr":
+            if multiclass:
+                return OneVsRestClassifier(linear_model.LogisticRegression(C=1e5)) 
+            else:
+                return linear_model.LogisticRegression(C=1e5)
+        elif name == "lsvm"
+            if multiclass:
+                return OneVsRestClassifier(LinearSVC(random_state=0))
+            else:
+                return LinearSVC(random_state=0)
+ 
+    def model_names(self):
+        return list(self.mods.keys())
+
+    def get_model(self, model_name):
+        return self.mods[model_name]
+
+    def is_multiclass():
+        return self.multiclass
--- run.py
+++ run.py
@@ -12,107 +12,91 @@ from scipy.sparse.csr import csr_matrix
 from sklearn.cross_validation import StratifiedKFold
 from sklearn import cross_validation
 from sklearn import metrics
-from sklearn.multiclass import OneVsRestClassifier
 
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC, LinearSVC
-from sklearn import linear_model
+import argparse
+
+parser = argparse.ArgumentParser(description='Used to training features.')
+parser.add_argument('--feature', metavar='F', nargs=1, help='specify file containing feature matrix')
+parser.add_argument('--label', metavar="L", nargs=1, help='specify file containing labels matrix')
+args = parser.parse_args()
+
 
 def load_sparse_csr(filename):
     loader = np.load(filename)
-    return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
-                         shape = loader['shape'])
+    return csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                       shape = loader['shape'])
+
 
 def mean(l):
     return reduce(lambda x, y: x + y, l) / len(l)
 
-algorithms = ["nb", "svm",  "lsvm", "lr"]
-if len(sys.argv) > 1:
-  algorithms = sys.argv[1:]
 
-data_file_processed = './tmp/data.bigram.temp.npz'
-target_file_processed = './tmp/target.bigram.temp.npy'
-
-multiclass = True
-print "Loading from file..."
-X = load_sparse_csr(data_file_processed);
-Y = np.load(target_file_processed)
-print "File Loaded"
-
-#split dataset to training set and test set
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X, Y, test_size=0.4, random_state=0)
-
-#initialization
-clfs = {}
-if multiclass:
-    clfs["nb"] = MultinomialNB()
-    clfs["svm"] = OneVsRestClassifier(SVC())
-    clfs["lsvm"] = OneVsRestClassifier(LinearSVC(random_state=0))
-    clfs["lr"] = OneVsRestClassifier(linear_model.LogisticRegression(C=1e5))
-else:
-    clfs["nb"] = MultinomialNB()
-    clfs["svm"] = SVC()
-    clfs["lsvm"] = LinearSVC(random_state=0)
-    clfs["lr"] = linear_model.LogisticRegression(C=1e5)
-
-meta = {}
-for a in algorithms:
-    meta[a] = {}
-    meta[a]["accuracy"] = []
-    meta[a]["precision"] = []
-    meta[a]["recall"] = []
-    meta[a]["fscore"] = []
-
-write_to_file = True
-files = {}
-for a in algorithms:
-    filename = "./reports/" + a + ".unigram.report"
-    files[a] = open(filename, "a+")
-
-#cross validation against training dataset
-skf = StratifiedKFold(y_train, n_folds=10, shuffle=True)
-for train_index, test_index in skf:
-    x, xt = X_train[train_index], X_train[test_index]
-    y, yt = y_train[train_index], y_train[test_index]
-    #test each algorithm
-    for a in algorithms:
-        clfs[a].fit(x, y)
-        yp = clfs[a].predict(xt)
-        meta[a]["report"] = metrics.classification_report(yt, yp);
-        if write_to_file:
-            files[a].write(meta[a]["report"])
-        meta[a]["accuracy"].append(metrics.accuracy_score(yt, yp))
-        meta[a]["precision"].append(metrics.precision_score(yt, yp, average='weighted'))
-        meta[a]["recall"].append(metrics.recall_score(yt, yp, average='weighted'))
-        meta[a]["fscore"].append(metrics.f1_score(yt, yp, average='weighted'))
-
-#close file
-for a in algorithms:
-    files[a].close()
-
-#print calssification result
-for a in algorithms:
-    print a
-    print "*****"
-    print "Accuracy:",
-    print mean(meta[a]["accuracy"])
-    print "Precison:",
-    print mean(meta[a]["precision"])
-    print "Recall:",
-    print mean(meta[a]["recall"])
-    print "F Score:",
-    print mean(meta[a]["fscore"])
+def generate_train_test_set(feature, label):
+    X = load_sparse_csr(feature);
+    Y = np.load(label)
+    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
+    return (X_train, X_test, Y_train, Y_test)
 
 
-#run against test dataset
-for a in algorithms:
-    print a
-    print "*****"
-    clfs[a].fit(X_train, y_train)
-    yp = clfs[a].predict(X_test)
-    print metrics.classification_report(y_test, yp)
-    print metrics.accuracy_score(y_test, yp)
-    print metrics.precision_score(y_test, yp, average='weighted')
-    print metrics.recall_score(y_test, yp, average='weighted')
-    print metrics.f1_score(y_test, yp, average='weighted')
+def create_meta(model_names):
+    meta = {}
+    for a in model_names:
+        meta[a] = {}
+        meta[a]["accuracy"] = []
+        meta[a]["precision"] = []
+        meta[a]["recall"] = []
+        meta[a]["fscore"] = []
+    return meta
+
+
+def train(models, meta, x_train, y_train):
+    #cross validation against training dataset
+    skf = StratifiedKFold(y_train, n_folds=10, shuffle=True)
+    for train_index, test_index in skf:
+        x, xt = x_train[train_index], x_train[test_index]
+        y, yt = y_train[train_index], y_train[test_index]
+        #test each algorithm
+        for m in models.model_names():
+	    model = models.get_model(m)
+            model.fit(x, y)
+            yp = model.predict(xt)
+            meta[m]["report"] = metrics.classification_report(yt, yp);
+            meta[m]["accuracy"].append(metrics.accuracy_score(yt, yp))
+            meta[m]["precision"].append(metrics.precision_score(yt, yp, average='weighted'))
+            meta[m]["recall"].append(metrics.recall_score(yt, yp, average='weighted'))
+            meta[m]["fscore"].append(metrics.f1_score(yt, yp, average='weighted'))
+
+
+def cpt_mean_score(meta):
+    for m in meta.keys():
+        print a
+        print "*****"
+        print "Accuracy:",
+        print mean(meta[m]["accuracy"])
+        print "Precison:",
+        print mean(meta[m]["precision"])
+        print "Recall:",
+        print mean(meta[m]["recall"])
+        print "F Score:",
+        print mean(meta[m]["fscore"])
+
+
+def test(models, x_test, y_test):
+    for m in models.model_names():
+	model = models.get_model(m)
+        model.fit(x_train, y_train)
+        yp = model.predict(y_test)
+        print metrics.classification_report(y_test, yp)
+        print metrics.accuracy_score(y_test, yp)
+        print metrics.precision_score(y_test, yp, average='weighted')
+        print metrics.recall_score(y_test, yp, average='weighted')
+        print metrics.f1_score(y_test, yp, average='weighted')
+
+def main():
+    model_names = ["nb", "svm",  "lsvm", "lr"]
+    model = Model(model_names)
+    meta = create_meta(model_name)
+    
+
+if __name__ == "__main__":
+    main()
