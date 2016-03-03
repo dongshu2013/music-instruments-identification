ccf14f9c7e02807add383b27986882074bf0837a
dongshu <dongs2011@gmail.com>
2016-03-02 18:24:01 -0600

    code refacotring

--- models.py
+++ models.py
@@ -7,10 +7,12 @@ class Models():
     mods = {}
     multiclass = True
 
+
     def __init__(self, models, multiclass=True):
         self.multiclass = multiclass
         for m in models:
            self.mods[m] = self.create_model(m, multiclass)
+
 
     def create_model(self, name, multiclass=True):
         if name == "nb":
@@ -19,7 +21,7 @@ class Models():
             return OneVsRestClassifier(SVC()) if multiclass else SVC()
         elif name == "lr":
             if multiclass:
-                return OneVsRestClassifier(linear_model.LogisticRegression(C=1e5)) 
+                return OneVsRestClassifier(linear_model.LogisticRegression(C=1e5))
             else:
                 return linear_model.LogisticRegression(C=1e5)
         elif name == "lsvm"
@@ -27,12 +29,15 @@ class Models():
                 return OneVsRestClassifier(LinearSVC(random_state=0))
             else:
                 return LinearSVC(random_state=0)
- 
+
+
     def model_names(self):
         return list(self.mods.keys())
 
+
     def get_model(self, model_name):
         return self.mods[model_name]
+
 
     def is_multiclass():
         return self.multiclass
--- run.py
+++ run.py
@@ -14,12 +14,14 @@ from sklearn import cross_validation
 from sklearn import metrics
 
 import argparse
+from models import Models
 
 parser = argparse.ArgumentParser(description='Used to training features.')
 parser.add_argument('--feature', metavar='F', nargs=1, help='specify file containing feature matrix')
 parser.add_argument('--label', metavar="L", nargs=1, help='specify file containing labels matrix')
 args = parser.parse_args()
 
+FOLD = 10
 
 def load_sparse_csr(filename):
     loader = np.load(filename)
@@ -34,69 +36,69 @@ def mean(l):
 def generate_train_test_set(feature, label):
     X = load_sparse_csr(feature);
     Y = np.load(label)
-    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
-    return (X_train, X_test, Y_train, Y_test)
+    return cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
 
 
-def create_meta(model_names):
-    meta = {}
-    for a in model_names:
-        meta[a] = {}
-        meta[a]["accuracy"] = []
-        meta[a]["precision"] = []
-        meta[a]["recall"] = []
-        meta[a]["fscore"] = []
-    return meta
-
-
-def train(models, meta, x_train, y_train):
+def train(models, x_train, y_train):
+    train_result = []
     #cross validation against training dataset
-    skf = StratifiedKFold(y_train, n_folds=10, shuffle=True)
+    skf = StratifiedKFold(y_train, n_folds=FOLD, shuffle=True)
     for train_index, test_index in skf:
         x, xt = x_train[train_index], x_train[test_index]
         y, yt = y_train[train_index], y_train[test_index]
         #test each algorithm
+        meta = {}
         for m in models.model_names():
-	    model = models.get_model(m)
+            model = models.get_model(m)
             model.fit(x, y)
             yp = model.predict(xt)
-            meta[m]["report"] = metrics.classification_report(yt, yp);
-            meta[m]["accuracy"].append(metrics.accuracy_score(yt, yp))
-            meta[m]["precision"].append(metrics.precision_score(yt, yp, average='weighted'))
-            meta[m]["recall"].append(metrics.recall_score(yt, yp, average='weighted'))
-            meta[m]["fscore"].append(metrics.f1_score(yt, yp, average='weighted'))
+            meta[m]["report"] = metrics.classification_report(yt, yp)
+            meta[m]["accuracy"] = metrics.accuracy_score(yt, yp)
+            meta[m]["precision"] = metrics.precision_score(yt, yp, average='weighted')
+            meta[m]["recall"] = metrics.recall_score(yt, yp, average='weighted')
+            meta[m]["fscore"] = metrics.f1_score(yt, yp, average='weighted')
+        train_result.append(meta)
+    return train_result
 
 
-def cpt_mean_score(meta):
+def print_meta(meta):
     for m in meta.keys():
-        print a
-        print "*****"
-        print "Accuracy:",
-        print mean(meta[m]["accuracy"])
-        print "Precison:",
-        print mean(meta[m]["precision"])
-        print "Recall:",
-        print mean(meta[m]["recall"])
-        print "F Score:",
-        print mean(meta[m]["fscore"])
+        print m
+        print "****"
+        print "Report:"
+        print meta[m]['report'][i]
+        print "Accuracy:"
+        print meta[m]['accuracy'][i]
+        print "Precision:"
+        print meta[m]['precision'][i]
+        print "Recall:"
+        print meta[m]['recall'][i]
+        print "F-Score:"
+        print meta[m]['fscore'][i]
 
 
-def test(models, x_test, y_test):
+def test(models, x_train, y_train, x_test, y_test):
+    test_result = {}
     for m in models.model_names():
-	model = models.get_model(m)
+        model = models.get_model(m)
         model.fit(x_train, y_train)
         yp = model.predict(y_test)
-        print metrics.classification_report(y_test, yp)
-        print metrics.accuracy_score(y_test, yp)
-        print metrics.precision_score(y_test, yp, average='weighted')
-        print metrics.recall_score(y_test, yp, average='weighted')
-        print metrics.f1_score(y_test, yp, average='weighted')
+        test_result[m]["report"] = metrics.classification_report(y_test, yp)
+        test_result[m]["accuracy"] = metrics.accuracy_score(y_test, yp)
+        test_result[m]["precision"] = metrics.precision_score(y_test, yp, average='weighted')
+        test_result[m]["recall"] = metrics.recall_score(y_test, yp, average='weighted')
+        test_result[m]["fscore"] = metrics.f1_score(y_test, yp, average='weighted')
+    return test_result
+
 
 def main():
+    x_train, x_test, y_train, y_test = generate_train_test_set(args['feature'], args['label'])
     model_names = ["nb", "svm",  "lsvm", "lr"]
     model = Model(model_names)
-    meta = create_meta(model_name)
-    
+    train_result = train(modle, x_train, y_train)
+    for meta in train_result:
+        print_meta(meta)
+
 
 if __name__ == "__main__":
     main()
